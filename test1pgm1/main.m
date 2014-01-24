9c0568e975b0dff36e277865b55edfc7c635cd3f
Samuel Tolkin <samueltolkin@Samuel-Tolkins-MacBook.local>
2014-01-24 10:05:48 -0500

    Initial Commit

---
+++ test1pgm1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test1pgm1
+//
+//  Created by Samuel Tolkin on 1/24/14.
+//  Copyright (c) 2014 Samuel Tolkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "test1pgm1AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([test1pgm1AppDelegate class]));
+    }
+}
---
+++ test1pgm1/test1pgm1ViewController.m
@@ -0,0 +1,29 @@
+//
+//  test1pgm1ViewController.m
+//  test1pgm1
+//
+//  Created by Samuel Tolkin on 1/24/14.
+//  Copyright (c) 2014 Samuel Tolkin. All rights reserved.
+//
+
+#import "test1pgm1ViewController.h"
+
+@interface test1pgm1ViewController ()
+
+@end
+
+@implementation test1pgm1ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
